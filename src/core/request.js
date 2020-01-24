44ffedc61ad528037b96feae511e103443d1c639
David Recuenco <david.recuenco@adidas-group.com>
2020-01-24 14:39:11 +0100

    USE available `Curl.option` values

--- src/core/request.js
+++ src/core/request.js
@@ -2,6 +2,9 @@ const { Curl } = require('node-libcurl');
 const HttpStatus = require('http-status-codes');
 const { fromMapToRawList, fromObjectToMap } = require('../util/headers');
 
+// `node-libcurl` supports most curl options, reference for the available options
+// can be found at https://curl.haxx.se/libcurl/c/curl_easy_setopt.html
+
 const httpVersions = Object.freeze({
   'default': 'CURL_HTTP_VERSION_NONE',
   'HTTP/1.0': 'CURL_HTTP_VERSION_1_0',
@@ -19,20 +22,20 @@ async function request({ version, method, url, headers, body }, flags) {
     const httpVersion = httpVersions[version ? version.toUpperCase() : 'default'];
 
     curl.setOpt(Curl.option.HTTP_VERSION, httpVersion);
-    curl.setOpt('CUSTOMREQUEST', method);
-    curl.setOpt('URL', url);
-    curl.setOpt('FOLLOWLOCATION', flags.location);
-    curl.setOpt('SSL_VERIFYPEER', !flags.insecure);
+    curl.setOpt(Curl.option.CUSTOMREQUEST, method);
+    curl.setOpt(Curl.option.URL, url);
+    curl.setOpt(Curl.option.FOLLOWLOCATION, flags.location);
+    curl.setOpt(Curl.option.SSL_VERIFYPEER, !flags.insecure);
 
     if (headers.size) {
       curl.setOpt(Curl.option.HTTPHEADER, fromMapToRawList(headers));
     }
 
     if (body) {
-      curl.setOpt('POSTFIELDS', body);
+      curl.setOpt(Curl.option.POSTFIELDS, body);
     }
 
-    // Omit result from headers
+    // Omit `result` from response headers
     // eslint-disable-next-line no-unused-vars
     curl.on('end', (statusCode, data, [{ result, ...headers }]) => {
       curl.close();
